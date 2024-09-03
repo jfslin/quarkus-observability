24c03ff090abd700024080e690811b999850e09f
Jonathan Lin <jlin@lvlmotion.com>
2024-09-03 17:34:09 -0400

    trying to get data streamed to grafana

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,7 +1,10 @@
 package org.acme;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -28,9 +31,21 @@ public class GreetingResource {
                 .register(registry);
     }
 
+    private final LongCounter counter;
+
+    public GreetingResource(Meter meter) {
+        counter = meter.counterBuilder("hello-metrics")
+                .setDescription("hello-metrics")
+                .setUnit("invocations")
+                .build();
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        counter.add(1);
+        log.info("hello-metrics");
+
         return "Hello from Quarkus REST";
     }
 }
